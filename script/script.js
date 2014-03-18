5a68b0c4895c8bd09104ac56168b92dcfa4dec14
bozmen <ozmen.brk@gmail.com>
2014-03-18 18:17:22 +0100

    Final touch on qualifications
    Only the quote part left.

--- script/script.js
+++ script/script.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
-	$("#education p").clicktoggle(function() {
+	$(".btn").clicktoggle(function() {
   		$(this).next().slideDown();
+  		verticallyAlign();
 	}, function() {
+		verticallyAlign();
   		$(this).next().slideUp();
 	});
+
+/*	// Vertically aligning languages
+	var a = $(".list");
+	for(i = 0; i < a.length; i++){
+		var height = $(a[i]).css('height');
+		height = extractIntFromPx(height);
+		$(a[i]).css('margin-top', -height/2);
+	};
+
+	// Vertically aligning "can"s
+	var b = $(".can");
+	for(j = 0; j < b.length; j++){
+		var height = $(b[j]).css('height');
+		height = extractIntFromPx(height);
+		var fontSize = $(b[j]).css('font-size');
+		fontSize = extractIntFromPx(fontSize);
+		$(b[j]).css('line-height', height/fontSize);
+	} */
 });
+
 
 $.fn.clicktoggle = function(a, b) {
     return this.each(function() {
@@ -19,4 +40,28 @@ $.fn.clicktoggle = function(a, b) {
         });
     });
 };
+
+var extractIntFromPx = function(string){
+	var k = string.indexOf('px');
+	string = parseInt(string.substring(0,k));
+	return string;
+}
 	
+var verticallyAlign = function(){
+	var a = $(".list");
+	for(i = 0; i < a.length; i++){
+		var height = $(a[i]).css('height');
+		height = extractIntFromPx(height);
+		$(a[i]).css('margin-top', -height/2);
+	};
+
+	// Vertically aligning "can"s
+	var b = $(".can");
+	for(j = 0; j < b.length; j++){
+		var height = $(b[j]).css('height');
+		height = extractIntFromPx(height);
+		var fontSize = $(b[j]).css('font-size');
+		fontSize = extractIntFromPx(fontSize);
+		$(b[j]).css('line-height', height/fontSize);
+	}
+}
