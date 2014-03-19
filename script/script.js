07e2c220bcd001613e568b1db63c8e0663408d40
bozmen <ozmen.brk@gmail.com>
2014-03-19 04:19:25 +0100

    Sliding links
    Really necessary?

--- script/script.js
+++ script/script.js
@@ -7,26 +7,16 @@ $(document).ready(function(){
   		$(this).next().slideUp();
 	});
 
-/*	// Vertically aligning languages
-	var a = $(".list");
-	for(i = 0; i < a.length; i++){
-		var height = $(a[i]).css('height');
-		height = extractIntFromPx(height);
-		$(a[i]).css('margin-top', -height/2);
-	};
+	alignQuote();
 
-	// Vertically aligning "can"s
-	var b = $(".can");
-	for(j = 0; j < b.length; j++){
-		var height = $(b[j]).css('height');
-		height = extractIntFromPx(height);
-		var fontSize = $(b[j]).css('font-size');
-		fontSize = extractIntFromPx(fontSize);
-		$(b[j]).css('line-height', height/fontSize);
-	} */
+	$("#right-personal td").mouseenter(function(){
+		$(this).find("span").stop(1,0).animate({"right": "0px"}, 350)
+	}).mouseleave(function(){
+		$(this).find("span").stop(1,0).animate({"right": "200px"}, 350)
+	});
 });
 
-
+// toggle function
 $.fn.clicktoggle = function(a, b) {
     return this.each(function() {
         var clicked = false;
@@ -45,9 +35,12 @@ var extractIntFromPx = function(string){
 	var k = string.indexOf('px');
 	string = parseInt(string.substring(0,k));
 	return string;
-}
+};
 	
+
+// alligning skills
 var verticallyAlign = function(){
+	// Vertically aligning skills
 	var a = $(".list");
 	for(i = 0; i < a.length; i++){
 		var height = $(a[i]).css('height');
@@ -64,4 +57,18 @@ var verticallyAlign = function(){
 		fontSize = extractIntFromPx(fontSize);
 		$(b[j]).css('line-height', height/fontSize);
 	}
-}
+};
+
+//alligning quote
+var alignQuote = function(){
+	var height = $('#aligner').css('height');
+	height = parseInt(height);
+	$('#aligner').css('margin-top', -(height/2));
+};
+
+//Changing quote
+var changeQuote = function(){
+};
+
+setInterval(changeQuote, 5000);
+
